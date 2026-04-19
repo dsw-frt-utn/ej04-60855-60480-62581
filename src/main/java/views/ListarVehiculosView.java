5e5ae1de46c8270a20878f84a704f049f27a6d97
kira25-sys <fontsfelipe77@gmail.com>
2026-04-19 11:55:39 -0300

    cree la clase marca y la integre con el codigo

--- src/main/java/views/ListarVehiculosView.java
+++ src/main/java/views/ListarVehiculosView.java
@@ -19,12 +19,14 @@ public class ListarVehiculosView extends javax.swing.JFrame {
     private void listarVehiculos(){
         ArrayList<VehiculoViewModel> vehiculos = Controlador.getVehiculos();
         vehiculosGrid.setModel(new DefaultTableModel(new Object[][] {}, 
-            new String[] { "Patente","Vehículo", "Tipo", "Sucursal", "Cap.Carga", "Km/litro", "Año", "Litros extra", "Km a recorrer" }));
+            new String[] { "Patente","Vehículo","Marca","Pais", "Tipo", "Sucursal", "Cap.Carga", "Km/litro", "Año", "Litros extra", "Km a recorrer" }));
         
         for(VehiculoViewModel vehiculo : vehiculos){
             ((DefaultTableModel)vehiculosGrid.getModel()).addRow(new Object[] {
                 vehiculo.getPatente(),
                 vehiculo.getVehiculo(),
+                vehiculo.getmarca(),
+                vehiculo.getpais(),
                 vehiculo.getTipo(),
                 vehiculo.getSucursal(),
                 vehiculo.getCapacidadCarga(),
@@ -158,7 +160,7 @@ public class ListarVehiculosView extends javax.swing.JFrame {
          TableModel table = vehiculosGrid.getModel();
          Map<String, Double> lista = new HashMap<>();
          for(int i=0;i< table.getRowCount();i++){
-             lista.put((String)table.getValueAt(i, 0), (Double)table.getValueAt(i, 8));
+             lista.put((String)table.getValueAt(i, 0), (Double)table.getValueAt(i, 10));
          }
          double[] consumos = Controlador.calcularConsumos(lista);
          totalConsumoElectricosValue.setText(String.format("%.2f%n kWh", consumos[0]));
